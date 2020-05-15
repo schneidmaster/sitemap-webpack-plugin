06419260f3abb019924c8882b5ec02592b21d508
Zach Schneider <zach@schneider.dev>
2020-05-15 13:59:15 -0500

    Sitemap index support (#32)
    * Working implementation of sitemap index for more than 50k paths
    
    * Use much faster libraries for converting stream > string > asset
    
    * Account for non-default public path and test skipping gzip
    
    * Bump version and add note in changelog
    
    * Remove unneeded extra time for many paths test
    
    * Implement a small stringToStream helper instead of a new dependency

---
+++ test/success-cases/many-paths-public-path/webpack.config.js
@@ -0,0 +1,22 @@
+import SitemapPlugin from "../../../src/";
+import { StatsWriterPlugin } from "webpack-stats-plugin";
+
+const paths = [];
+for (let i = 0; i < 60000; i++) {
+  paths.push(`/${i}`);
+}
+
+export default {
+  entry: () => [],
+  output: {
+    filename: "index.js",
+    path: `${__dirname}/actual-output`,
+    libraryTarget: "umd",
+    publicPath: "/assets"
+  },
+
+  plugins: [
+    new SitemapPlugin("https://mysite.com", paths),
+    new StatsWriterPlugin() // Causes the asset's `size` method to be called
+  ]
+};
