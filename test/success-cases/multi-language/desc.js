c48848bcde651553848c07487f21f91318f1d1c9
Zach Schneider <zach@schneider.dev>
2020-07-16 11:54:17 -0500

    Add test demonstrating hreflang multi-language support

---
+++ test/success-cases/multi-language/desc.js
@@ -0,0 +1,1 @@
+export default "Sitemap with a mix of global and path-specific options";
---
+++ test/success-cases/multi-language/webpack.config.js
@@ -0,0 +1,47 @@
+import SitemapPlugin from "../../../src/";
+
+const paths = [
+  {
+    path: "/",
+    lastMod: "2016-01-01",
+    changeFreq: "daily",
+    priority: "1.0",
+    links: [
+      { lang: "en", url: "/" },
+      { lang: "es", url: "/es/" }
+    ]
+  },
+  {
+    path: "/about/",
+    priority: "0.4",
+    links: [
+      { lang: "en", url: "/" },
+      { lang: "es", url: "/es/about/" }
+    ]
+  },
+  {
+    path: "/faq/",
+    links: [
+      { lang: "en", url: "/" },
+      { lang: "es", url: "/es/faq/" }
+    ]
+  },
+  {
+    path: "/contact/",
+    links: [
+      { lang: "en", url: "/" },
+      { lang: "es", url: "/es/contact/" }
+    ]
+  }
+];
+
+export default {
+  entry: () => [],
+  output: {
+    filename: "index.js",
+    path: `${__dirname}/actual-output`,
+    libraryTarget: "umd"
+  },
+
+  plugins: [new SitemapPlugin("https://mysite.com", paths)]
+};
