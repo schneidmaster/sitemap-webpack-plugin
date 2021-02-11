94dc515f04d866cb60d2ccd60b3116c1b203220a
Zach Schneider <zach@schneider.dev>
2021-02-11 14:11:32 -0600

    More realistic generation failure test for malformed base URL (#47)

--- test/errors.test.ts
+++ test/errors.test.ts
@@ -1,23 +1,29 @@
 import webpack from "webpack";
 import webpackConfig from "./cases/basic/webpack.config";
-import * as generators from "../src/generators";
+import SitemapPlugin from "../src";
 import * as helpers from "../src/helpers";
 
 describe("Errors", () => {
-  it("reports generation error", done => {
-    const spy = jest
-      .spyOn(generators, "generateSitemaps")
-      .mockImplementation(() => {
-        throw new Error("a generation error happened");
-      });
-
-    webpack(webpackConfig, (_err, output) => {
-      expect(output?.compilation?.errors[0]).toEqual(
-        expect.stringContaining("Error: a generation error happened")
-      );
-      spy.mockRestore();
-      done();
-    });
+  it("reports generation error from malformed base URL", done => {
+    webpack(
+      {
+        ...webpackConfig,
+        plugins: [
+          new SitemapPlugin({
+            base: "example.com",
+            paths: ["/", "/about"]
+          })
+        ]
+      },
+      (_err, output) => {
+        expect(output?.compilation?.errors[0]).toEqual(
+          expect.stringContaining(
+            "TypeError [ERR_INVALID_URL]: Invalid URL: example.com"
+          )
+        );
+        done();
+      }
+    );
   });
 
   it("reports gzip error", done => {
